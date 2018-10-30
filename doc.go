7ebf4a98c4f8fa4f7c39cfafdb848d6af52e7c55
Шестаков Денис <shestakovda@gmail.com>
2018-10-30 22:31:01 +0300

    [add]: New interfaces
    Rethinking and renaming to conform the code convention

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package wpgx helps to improve loading performance and to simplify code.
+//
+// TODO: usage
+package wpgx
--- interfaces.go
+++ interfaces.go
@@ -1,5 +1,68 @@
 package wpgx
 
+// Translator helps to isolate struct from loading procedure
+//
+// Translate is intended to associate structure fields with their names
+type Translator interface {
+	Translate(name string) interface{}
+}
+
+// Collector is a generic collection. It can use lists, maps or channels inside
+//
+// NewItem prepares a translator entity for future loads
+//
+// Collect puts a new translator item into the inside collection
+type Collector interface {
+	NewItem() Translator
+	Collect(item Translator)
+}
+
+// Dealer is an active subject, like an opened transaction, for query performing
+//
+// Deal! It executes query and loads result into a data collector. Pass nil when no result needed
+//
+// Load gets just one item from the database. When no collection needed
+//
+// Save inserts item into database. Result may need for getting new ID or properties
+//
+// Jail (aka Close) ends a transaction with commit or rollback respective to the flag
+type Dealer interface {
+	Deal(result Collector, query string, args ...interface{}) error
+
+	Load(item Translator, query string, args ...interface{}) error
+	Save(item Translator, query string, result Collector) error
+
+	Jail(commit bool) error
+}
+
+// Connector is the main database connection manager
+// As a Dealer it can execute queries in a default transaction
+//
+// NewDealer spawns new dealer on the street. It needs to be jailed (closed)
+//
+// Reserve path is for saving args of failed queries. Useful for debug or data restore
+//
+// Register saves query for further preparation. Must be called before Connect
+//
+// Connect method initialize a new connection pool with uri in a connection string format
+//
+// Close closes all free dealers with rollback
+type Connector interface {
+	Dealer
+
+	NewDealer() Dealer
+
+	Reserve(path string) error
+
+	Register(query string, names []string) string
+
+	Connect(uri string) error
+
+	Close()
+}
+
+/******************************************************************************/
+
 // IListItem - Интерфейс для работы с элементом данных в БД
 type IListItem interface {
 
